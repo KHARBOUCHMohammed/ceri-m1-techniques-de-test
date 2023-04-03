c9afb397e111c1e449c6cc5e744e0a26d5c8d869
KHARBOUCHMohammed <kharbouchemohammed@gmail.com>
2023-04-03 15:32:15 +0200

    test 13

--- src/test/java/fr/univavignon/pokedex/api/IPokedexFactoryTest.java
+++ src/test/java/fr/univavignon/pokedex/api/IPokedexFactoryTest.java
@@ -3,10 +3,35 @@ import static org.junit.Assert.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
-
-import org.junit.Before;
 import org.junit.Test;
 import org.mockito.invocation.InvocationOnMock;
 import org.mockito.stubbing.Answer;
 public interface IPokedexFactoryTest {
+
+    IPokedexFactory pokedexFactory = mock(IPokedexFactory.class);
+    IPokemonMetadataProvider metadataProvider =  mock(IPokemonMetadataProvider.class);
+    IPokemonFactory pokemonFactory = mock(IPokemonFactory.class);
+
+    @Test
+
+
+
+
+
+    public default void creationPokedex() {
+        when(pokedexFactory.createPokedex(any(), any())).thenAnswer(new Answer<IPokedex>() {
+            public IPokedex answer(InvocationOnMock invocation) {
+                IPokedex pokedex = mock(IPokedex.class);
+                when(pokedex.size()).thenReturn(0);
+                return pokedex;
+            }
+        });
+
+
+        IPokedex pokedex =  pokedexFactory.createPokedex(metadataProvider, pokemonFactory);
+        assertEquals(0, pokedex.size());
+
+
+    }
+
 }
